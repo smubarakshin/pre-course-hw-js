194f5ba92516c90e5bf96a83032cce0d42b85f41
Shamil Mubarakshin <shamilmubarakshin@Shamils-MacBook-Pro.local>
2023-11-08 13:59:26 -0500

    new update to lesson 5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -9,7 +9,7 @@ switch (true) {
      console.log("У меня 100 пудов все получится"); 
      break;
 
-     case rule.length == 12.5: 
+     case rule.length === 25: 
      console.log("50 на 50"); 
      break;
      
