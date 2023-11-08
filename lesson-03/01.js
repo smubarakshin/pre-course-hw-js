1fa6dd192e431f27b84e80ffa725c8c542e373df
Shamil Mubarakshin <shamilmubarakshin@Shamils-MacBook-Pro.local>
2023-11-08 10:32:50 -0500

    resolution of lesson 3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,16 @@
-// code
+let number = 1;
+
+console.log(number)
+
+while(number != 3) {
+    ++number
+}
+
+console.log(number)
+console.log(number)
+
+while(number !=1) {
+    --number
+}
+
+console.log(number)
